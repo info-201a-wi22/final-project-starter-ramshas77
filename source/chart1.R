46885857cbe812a9ab3c40bdacbe7e64e4f54262
Naomi Moss <ch3rry@uw.edu>
2022-02-15 18:05:34 -0800

    new chart1 changes

--- source/chart1.R
+++ source/chart1.R
@@ -1,1 +1,7 @@
-# for naomi's chart - links titles with the word "diagnosis" in them and the number of comments they have 
+# for naomi's chart - links keywords and the number of comments they have 
+
+adhd_data <- read.csv("C:/Users/platy/Documents/Info201_code/ADHD.csv")
+
+max_comments <- max(adhd_data$num_comments, na.rm = TRUE)
+
+range_comments <- adhd_data$num_comments
