50c2d206f8d768b45e9b65f00072d3eaf0c0147a
Naomi Moss <ch3rry@uw.edu>
2022-02-15 23:55:38 -0800

    chart made

--- source/chart1.R
+++ source/chart1.R
@@ -2,7 +2,8 @@
 
 library(dplyr)
 library(stringr)
-
+library(ggplot2)
+library(plotly)
 
 adhd_data <- read.csv("C:/Users/platy/Documents/Info201_code/ADHD.csv")
 
@@ -13,23 +14,39 @@ top_101 <- adhd_data %>%
   filter(num_comments > 421)
 
 find_word <- top_101 %>%
-  filter(str_detect(.$selftext, ""))
+  filter(str_detect(.$selftext, "today"))
 
 variable <- nrow(find_word)
 
-# been searching all data based on keywords, medication appears ~12600 times, help appears ~12000 times, diagnose appears ~9000 times, focus appears ~6000 times, adhd appears ~6700 times
+term <- c('help', 
+          'medication', 
+          'focus', 
+          'hard', 
+          'struggle', 
+          'forget', 
+          'adhd', 
+          'sleep', 
+          'doctor', 
+          'diagnose', 
+          'plan', 
+          'symptoms'
+          )
 
-# after searching only the top 101 posts with the most comments, 
-# help appears 20 times
-# medication appears 16 times
-# focus appears 16 times
-# hard appears 15 times
-# struggle appears 14 times
-# forget appears 12 times
-# adhd appears 11 times
-# sleep appears 11 times
-# doctor appears 10 times
-# diagnose appears 9 times
-# plan appears 9 times
-# symptoms appears 7 times
-# 
+frequency <- c(20, 
+               16, 
+               16, 
+               15, 
+               14, 
+               12, 
+               11, 
+               11, 
+               10, 
+               9, 
+               9, 
+               7
+               )
+
+term_frequencies <- data.frame(term, frequency) 
+
+ggplot(data = term_frequencies) +
+  geom_col( mapping = aes(x = term, y = frequency, color = term))
