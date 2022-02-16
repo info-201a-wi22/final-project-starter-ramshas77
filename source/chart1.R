5f0bb6497a8c1bd62600a8049f8b38d3cc331431
Naomi Moss <ch3rry@uw.edu>
2022-02-15 18:44:25 -0800

    chart1 changes

--- source/chart1.R
+++ source/chart1.R
@@ -4,4 +4,15 @@ adhd_data <- read.csv("C:/Users/platy/Documents/Info201_code/ADHD.csv")
 
 max_comments <- max(adhd_data$num_comments, na.rm = TRUE)
 
-range_comments <- adhd_data$num_comments
+diagnose_comments <- adhd_data %>%
+  select(title, selftext, num_comments) %>%
+  filter(num_comments > 421)%>%
+  filter(str_detect(.$title, "anxiety"))
+
+diagnose <- nrow(diagnose_comments)
+
+# been searching all data based on keywords, medication appears ~12600 times, help appears ~12000 times,
+# diagnose appears ~9000 times, focus appears ~6000 times, adhd appears ~6700 times
+
+# after searching only the top 101 posts with the most comments, medication appears once, help appears 4 times,
+# diagnose appears once, focus appears 4 times, adhd appears twice
