fe68236d293324e21c6439d8b3cc60da6dd3bf5e
Naomi Moss <ch3rry@uw.edu>
2022-02-15 22:57:09 -0800

    new values

--- source/chart1.R
+++ source/chart1.R
@@ -1,19 +1,35 @@
 # for naomi's chart - the number of times certain keywords appear in posts. 
 
+library(dplyr)
+library(stringr)
+
+
 adhd_data <- read.csv("C:/Users/platy/Documents/Info201_code/ADHD.csv")
 
 max_comments <- max(adhd_data$num_comments, na.rm = TRUE)
 
-diagnose_comments <- adhd_data %>%
+top_101 <- adhd_data %>%
   select(title, selftext, num_comments) %>%
-  filter(num_comments > 220)%>% # 421 gets top 101 posts, 299 gets top 300 posts, 220 gets top 650 poses
-  filter(str_detect(.$title, "doctor"))
+  filter(num_comments > 421)
 
-aaa <- nrow(diagnose_comments)
+find_word <- top_101 %>%
+  filter(str_detect(.$selftext, ""))
+
+variable <- nrow(find_word)
 
 # been searching all data based on keywords, medication appears ~12600 times, help appears ~12000 times, diagnose appears ~9000 times, focus appears ~6000 times, adhd appears ~6700 times
 
-# after searching only the top 101 posts with the most comments, medication appears once, help appears 4 times, diagnose appears once, focus appears 4 times, adhd appears twice
-# after searching only the top 300 posts with the most comments, medication appears 8 times, help appears 7 times, diagnose appears 6 times, focus appears 6 times, adhd appears 11 times, doctor appears 3 times
-# after searching only the top 650 posts with the most comments, medication appears 15 times, help appears 13 times, diagnose appears 13 times, focus appears 16 times, adhd appears 20 times, doctor appears 4 times
-
+# after searching only the top 101 posts with the most comments, 
+# help appears 20 times
+# medication appears 16 times
+# focus appears 16 times
+# hard appears 15 times
+# struggle appears 14 times
+# forget appears 12 times
+# adhd appears 11 times
+# sleep appears 11 times
+# doctor appears 10 times
+# diagnose appears 9 times
+# plan appears 9 times
+# symptoms appears 7 times
+# 
