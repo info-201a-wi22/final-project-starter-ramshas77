ac377936341042682c2fd5bda13500b58754ca93
sbains2 <97136840+sbains2@users.noreply.github.com>
2022-02-17 19:24:55 -0800

    Add files via upload

---
+++ source/All_Summary_and_Statistics.R
@@ -0,0 +1,51 @@
+# Our data includes four different datasets about ADHD posts on Reddit. The main dataset 
+# that we will be using is called "ADHD" and stores all of the posts made on the Reddit thread "ADHD."
+# This dataframe has 336066 rows and 8 columns.
+# One column in this dataframe stores the number of comments each post in "ADHD" has.
+# The post with the most comments -titled "/sigh"- has over 1.5 billion comments.
+
+# Our five statistics!
+
+# Statistic 1:
+# What is the title of the post with the most comments and how many comments does it have?
+max_comments <- max(adhd_data$num_comments, na.rm = TRUE)
+# 1505919691 comments
+max_titles <- adhd_data[adhd_data$num_comments == max_comments, "title"]
+# "/sigh"
+
+# Statistic 2:
+# How many times is ADHD mentioned in conjunction with the word "depression?"
+
+word_detect <- function(word) {
+  a <- sum(str_detect(adhd_data$body, word))
+  return(a)
+} 
+
+depression_occurrence <- word_detect("depression")
+
+#depression is mentioned 79340 times. 
+
+# Statistic 3:
+# How many times is ADHD mentioned in conjunction with the word "OCD?"
+
+word_detect <- function(word) {
+  a <- sum(str_detect(adhd_data$body, word))
+  return(a)
+} 
+
+ocd_occurrence <- word_detect("OCD")
+#ocd is mentioned 11832 times. 
+
+
+
+# Statistic 4:
+#How prevalent is the term 'medication' used across the data set?
+find_term <- sample_data %>% 
+  filter(str_detect(.$title, "medication"))
+
+var1 <- nrow(find_term)
+
+#The term medication is mentioned 430 times
+
+# Statistic 5: 
+
