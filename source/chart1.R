b8cc0b1039a77dec2bdfb67f98bdd8a275e21cf3
Naomi Moss <ch3rry@uw.edu>
2022-02-18 14:43:55 -0800

    index writing

--- source/chart1.R
+++ source/chart1.R
@@ -49,4 +49,4 @@ frequency <- c(20,
 term_frequencies <- data.frame(term, frequency) 
 
 terms_chart <- ggplot(data = term_frequencies) +
-  geom_col( mapping = aes(x = term, y = frequency, fill = term))
+  geom_col(mapping = aes(x = term, y = frequency))
